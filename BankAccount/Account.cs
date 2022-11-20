c3d394fd30bd394fa33a774d2aedd9bc214f741f
Filip Copija <filip.copija@microsoft.wsei.edu.pl>
2022-11-20 23:53:53 +0100

    It works, but logging is bad

--- BankAccount/Account.cs
+++ BankAccount/Account.cs
@@ -11,27 +11,26 @@ namespace BankAccount
         private string _accNumber;
         private string _password;
         private string _name;
-        private string _surname;
         private double _balance;
 
-        public Account(string name, string surname, string accountNumber, string password)
+        public Account(string name, string accountNumber, string password)
         {
             _name = name;
-            _surname = surname;
             _accNumber = accountNumber;
             _password = password;            
         }
 
         public string GetAccountNumber() => this._accNumber;
+        public double GetBalance() => this._balance;
         public string ShowBalance() => $"Hi {this._name}! Your account balance is {this._balance}.";
         public double Withdraw(double money)
         {
             if (this._balance >= money)
             {
                 this._balance = this._balance - money;
-                Console.WriteLine("The money was successfully withdrawn");
+                Console.WriteLine("The money was successfully withdrawn!");
             }
-            else Console.WriteLine("Such an operation is not possible");
+            else Console.WriteLine("Such an operation is not possible!");
 
             return this._balance;
         }
--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using BankAccount;
 
+// --- USER CREATE --- //
 List<string> history = new List<string>();
 
 Console.WriteLine("\t Welcome to StoneX Bank!");
@@ -11,59 +12,74 @@ Console.WriteLine();
 
 Console.WriteLine("Name:");
 var name = Console.ReadLine();
-Console.WriteLine("Surname:");
-var surname = Console.ReadLine();
 Console.WriteLine("Login:");
 var login = Console.ReadLine();
 Console.WriteLine("Password:");
 var password = Console.ReadLine();
 
-Account account = new Account(name, surname, login, password);
+Account account = new Account(name, login, password);
 Console.WriteLine("Succesfully added!");
 
 Thread.Sleep(1000);
 
 Console.Clear();
 
+// --- OPENS MENU --- //
 Console.WriteLine($"\t    | StoneX Bank Menu |");
 Console.WriteLine();
-Console.WriteLine($"\tHello {name} {surname}!");
+Console.WriteLine($"\tHello {name}!");
 Console.WriteLine();
 
+// --- NAVIGATION --- //
+menu:
 Console.WriteLine(
     "\n \t --- Choose an action ---" +
     "\n \t 1. Top up your balance" +
     "\n \t 2. Withdraw money" +
     "\n \t 3. Check your balance" +
     "\n \t 4. Transaction history" +
-    "\n \t 5. Exit");
+    "\n \t Press any other key to exit...");
 
 var input = Console.ReadKey();
 var key = input.KeyChar;
 int value;
+
+if (int.TryParse(key.ToString(), out value))
 {
-    if (int.TryParse(key.ToString(), out value))
+    switch (value)
     {
-        Console.WriteLine();
-        RouteChoice(value);
+        case 1:
+            Deposit(account, history);
+            goto menu;
+        case 2:
+            Withdraw(account, history);
+            goto menu;
+        case 3:
+            Check(account);
+            goto menu;
+        case 4:
+            ShowHistory(history);
+            goto menu;
+        default:
+            Console.WriteLine("Goodbye!");
+            break;
     }
-    else
-        Console.WriteLine("Invalid Entry.");
 }
-
-
-Console.Write("Press any key to exit...");
 Console.ReadKey(false);
-
 Console.ReadLine();
 
+// --- METHODS --- //
+void Check(Account account)
+{
+    Console.WriteLine(account.ShowBalance());
+}
 
 void Deposit(Account account, List<string> history)
 {
     Console.WriteLine("Deposit money: ");
     double cash = Double.Parse(Console.ReadLine());
     account.Deposit(cash);
-    history.Add($"|->|Deposited: {cash}. Current balance: {account.ShowBalance}");
+    history.Add($"|->|Deposited: {cash}. Current balance: {account.GetBalance()}.");
 }
 
 void Withdraw(Account account, List<string> history)
@@ -71,40 +87,11 @@ void Withdraw(Account account, List<string> history)
     Console.WriteLine("Withdraw moeny: ");
     double cash = Double.Parse(Console.ReadLine());
     account.Withdraw(cash);
-    history.Add($" |<-| Withdrawn: {cash}. Current balance: {account.ShowBalance}");
-}
-
-void Check(Account account)
-{
-    account.ShowBalance();
+    history.Add($"|<-| Withdrawn: {cash}. Current balance: {account.GetBalance()}.");
 }
 
 void ShowHistory(List<string> history)
 {
     foreach (var transaction in history)
         Console.WriteLine(transaction);
-}
-
-void RouteChoice(int menuChoice)
-{
-    switch (menuChoice)
-    {
-        case 1:
-            Deposit(account, history);
-              break;
-        case 2:
-            Withdraw(account, history);
-            break;
-        case 3:
-            Check(account);
-            break;
-        case 4:
-            ShowHistory(history);
-            break;
-        case 5:
-            break;
-        default:
-            Console.WriteLine("Invalid Entry!");
-            break;
-    }
 }

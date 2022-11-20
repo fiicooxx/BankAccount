571f5ae20b1c954df86908c288fda9a1486af021
Filip Copija <filip.copija@microsoft.wsei.edu.pl>
2022-11-20 22:06:51 +0100

    working on the interface

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System.Collections.Generic;
+using System.Threading;
+
+Console.WriteLine("\t Welcome to StoneX Bank!");
+Console.WriteLine("\t--- Create new user ---");
+Thread.Sleep(500);
+Console.WriteLine();
+
+Console.WriteLine("Name:");
+var name = Console.ReadLine();
+Console.WriteLine("Surname:");
+var surname = Console.ReadLine();
+Console.WriteLine("Login:");
+var login = Console.ReadLine();
+Console.WriteLine("Password:");
+var password = Console.ReadLine();
+
+Console.Clear();
+
